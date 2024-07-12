7434a57d3cdc52d2c89edc810d8963fd13e1b97a
DiegoJfsr <diegojfsr@gmail.com>
2024-07-12 14:50:32 -0300

    EstruturasCondicionais

---
+++ 10_EstruturasCondicionai.js
@@ -0,0 +1,28 @@
+
+
+// Estruturas Condicionais
+
+const a = 10;
+if (a > 8) {
+cosole.log("A e maior que 8.");
+}
+
+// >, <, >=, <=
+
+const b = "Diego";
+if(b === "Joao"){
+console.log("O nome e Joao");
+} else if (b === "Pedro") {
+console.log("O nome e Pedro")
+} else {
+console.log("Nao encontramos o nome do usuario.")
+}
+
+
+// if ternario
+
+const someNumber = 14;
+
+let testingNumber = someNumber < 20 ? "Yes" : "No";
+console.log(testingNumber);
+
