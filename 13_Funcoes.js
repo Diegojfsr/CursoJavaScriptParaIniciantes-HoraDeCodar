20ca5d422284048256c987b17c3a255f884dc459
DiegoJfsr <diegojfsr@gmail.com>
2024-07-13 21:31:07 -0300

    Funcoes

---
+++ 13_Funcoes.js
@@ -0,0 +1,30 @@
+
+// Funcoes
+
+function minhaFuncao(){
+    console.log("Ola Funcao");
+}
+minhaFuncao();
+
+//funcoes com argumentos - args
+function nomeCompleto(nome, sobrenome){
+    console.log(`O nome completo e: ${nome} ${sobrenome}`);
+}
+nomeCompleto("Diego", "Jeff");
+
+
+// funcoes com retorno - retun
+function nomeCompleto(nome, sobrenome){
+    retun `O nome completo e: ${nome} ${sobrenome}`
+}
+
+console.log(nomeCompleto("Diego", "Jeff"))
+console.log(nomeCompleto("Joao", "Azevedo"))
+
+const primeiroNome = "Maria";
+const segundoNome = "Matos";
+
+//const meuNomeCompleto = nomeCompleto("Maria", "Matos")
+const meuNomeCompleto = nomeCompleto(primeiroNome, segundoNome)
+console.log(meuNomeCompleto)
+    
